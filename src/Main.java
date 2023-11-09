35de6ec393c30fda9350443220395a22292050b6
bangdang <bang.dang.hai1411@gmail.com>
2023-11-09 22:12:46 +0700

    Init master

--- src/Main.java
+++ src/Main.java
@@ -18,6 +18,7 @@ public class Main extends JFrame {
     private JTextArea textInputConsole;
     private JScrollPane scrollPanel;
     private JButton generateButton;
+    private JRadioButton buttondinicSAlgorithm;
     private int selectAlgorithms = 0;
     Map<Integer, List<Integer>> sourceData = new HashMap<>();
 
@@ -33,6 +34,7 @@ public class Main extends JFrame {
             public void actionPerformed(ActionEvent e) {
                 selectAlgorithms = 1;
                 buttonEdmondsKarp.setSelected(false);
+                buttondinicSAlgorithm.setSelected(false);
             }
         });
         buttonEdmondsKarp.addActionListener(new ActionListener() {
@@ -40,8 +42,19 @@ public class Main extends JFrame {
             public void actionPerformed(ActionEvent e) {
                 selectAlgorithms = 2;
                 buttonFordF.setSelected(false);
+                buttondinicSAlgorithm.setSelected(false);
             }
         });
+
+        buttondinicSAlgorithm.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                selectAlgorithms = 3;
+                buttonFordF.setSelected(false);
+                buttonEdmondsKarp.setSelected(false);
+            }
+        });
+
         runAlgorithmButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -68,6 +81,7 @@ public class Main extends JFrame {
                 generateSourceData();
             }
         });
+
     }
 
     private void generateSourceData() {
@@ -81,17 +95,15 @@ public class Main extends JFrame {
             Random random = new Random();
 
             // Add 100 random numbers to the ArrayList
-            for (int j = 0; j < 100; j++) {
-                randomList.add(random.nextInt(10) + 1);
+            for (int j = 0; j < numberOfNodes; j++) {
+                if (j == i - 1 || (Math.abs(j - i + 1)) > 10) randomList.add(0);
+                else randomList.add(random.nextInt(10) + 1);
             }
             sourceData.put(i, randomList);
         }
         textInputConsole.setText("");
-        textInputConsole.append("Loading data test......");
-        textInputConsole.append("\nLoaded.");
+        textInputConsole.append("Generating data test......");
         textInputConsole.append("\nNumber of Nodes: " + numberOfNodes);
-        textInputConsole.append("\nSource: " + source);
-        textInputConsole.append("\nSink: " + sink);
         textInputConsole.append("\nGraph data:");
 
         for (Map.Entry<Integer, List<Integer>> sourceItem : sourceData.entrySet()) {
@@ -122,16 +134,16 @@ public class Main extends JFrame {
         sink = 100;
 
         FordFulkerson fordFulkerson = new FordFulkerson(numberOfNodes);
-        long startTime = System.currentTimeMillis();
+        long startTime = System.nanoTime();
         maxFlow = fordFulkerson.fordFulkerson(graph, source, sink);
-        long endTime = System.currentTimeMillis();
+        long endTime = System.nanoTime();
         long totalTime = endTime - startTime;
         StringBuilder message = new StringBuilder();
         message.append("[").append(formatter.get().format(new Date())).append("] ");
         textOutputConsole.append("\n" + message);
         textOutputConsole.append("\nRunning Ford-Fulkerson Algorithm......");
         textOutputConsole.append("\nMaximum flow is: " + maxFlow);
-        textOutputConsole.append("\nRunning time: " + totalTime + " milliseconds");
+        textOutputConsole.append("\nRunning time: " + (float)(totalTime/(float)1000000) + " milliseconds");
     }
 
     private void ActionEdmondsKarpAlgorithm() {
@@ -148,16 +160,16 @@ public class Main extends JFrame {
             }
         }
 
-        long startTime = System.currentTimeMillis();
+        long startTime = System.nanoTime();
         maxFlow = ek.getMaxFlow(0, 5);
-        long endTime = System.currentTimeMillis();
+        long endTime = System.nanoTime();
         long totalTime = endTime - startTime;
         StringBuilder message = new StringBuilder();
         message.append("[").append(formatter.get().format(new Date())).append("] ");
         textOutputConsole.append("\n" + message);
         textOutputConsole.append("\nRunning Edmonds-Karp Algorithm......");
         textOutputConsole.append("\nMaximum flow is: " + maxFlow);
-        textOutputConsole.append("\nRunning time: " + totalTime + " milliseconds");
+        textOutputConsole.append("\nRunning time: " + (float)(totalTime/(float)1000000) + " milliseconds");
     }
 
     public static void main(String[] args) {
